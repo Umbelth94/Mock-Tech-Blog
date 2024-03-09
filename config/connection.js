f8d95309cd0879d49d08925bcc05d8d86514113f
Travis Umbel <Umbelth94@gmail.com>
2024-03-09 15:59:04 -0600

    Server starts and reads "hello world"

--- config/connection.js
+++ config/connection.js
@@ -12,9 +12,17 @@ if (process.env.JAWSDB_URL){
         {
             host: 'localhost',
             dialect: 'mysql',
-            port: 3301
+            port: 3306
         }
     );
 }
 
+sequelize.authenticate()
+    .then(() => {
+        console.log('Connection has been established successfully.');
+    })
+    .catch(err => {
+        console.error('Unable to connect to the database:', err)
+    });
+    
 module.exports = sequelize;
