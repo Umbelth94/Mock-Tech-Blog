030dee6dc2ab437181348852763a344b222bdfe7
Travis Umbel <Umbelth94@gmail.com>
2024-03-09 18:11:20 -0600

    Data is seeded, home API route sends user data as JSON successfully

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,8 +1,17 @@
 const router = require('express').Router();
+const { User, Comment, BlogPost } = require ('../models');
 
+//Currently shows all blog posts (sends through the data)
+//Need to make this actually populate the home page
 router.get('/', async (req, res) => {
     try{
-        res.send('This is the home page');
+        const Blogdata = await User.findAll({
+            include: [
+                {model: BlogPost,},
+            ],
+        }
+        )
+        res.json(Blogdata);
     }
     catch (err) {
         res.status(500).json(err);
