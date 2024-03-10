a310b832639de33699e6dc03ddf27a03a61d3e21
Travis Umbel <Umbelth94@gmail.com>
2024-03-10 17:51:01 -0500

    dashboard populates with users posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const withAuth = require('../utils/auth');
 const { User, Comment, BlogPost } = require ('../models');
 
 //Currently shows all blog posts (sends through the data)
@@ -16,6 +17,28 @@ router.get('/', async (req, res) => {
         res.render('home', {blogPosts})
     }
     catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+router.get('/dashboard', withAuth, async (req, res) => {
+    try {
+        //Find the logged in user based on their session ID
+        const userData = await User.findByPk(req.session.user_id, {
+            attributes: {exclude: ['password'] },
+            include: [{ model: BlogPost}],
+        });
+
+        console.log(userData);
+
+        const user = userData.get({ plain: true });
+
+
+        res.render('dashboard', {
+            ...user,
+            logged_in: true
+        })
+    } catch (err) {
         res.status(500).json(err);
     }
 })
